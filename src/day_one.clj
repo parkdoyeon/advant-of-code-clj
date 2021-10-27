1eb1f93785c4aa9a695ef676b9f52234130f43df
parkdoyeon <>
2021-10-27 18:06:08 +0900

    Refactor day one

--- src/day_one.clj
+++ src/day_one.clj
@@ -2,6 +2,7 @@
    (:require [clojure.string :as str]))
 
 ; part 1
+; Use apply
 (->> "inputs/day-one.txt"
     slurp
     str/split-lines
@@ -9,32 +10,44 @@
     (apply +)
     )
 
+; Use reducer
 (->> "inputs/day-one.txt"
      slurp
      str/split-lines
      (reduce #(+ %1 (Integer/parseInt %2)) 0)
      )
 
-
 ; part 2
-; This needs an improvement since inputs always have to be checked not empty
-(defn reached_twice [inputs]
+; Use recur
+(defn loop_reached_twice [inputs]
   (loop [sum 0
-         iter_inputs inputs
+         inputs inputs
          reached #{}]
       (if (reached sum)
         sum
-        (recur (+ sum (first (if (empty? iter_inputs) inputs iter_inputs)))
-               (rest (if (empty? iter_inputs) inputs iter_inputs))
+        (recur (+ sum (first inputs))
+               (rest inputs)
                (conj reached sum))
         )))
+
+; Use reducer
+(defn reached_twice [inputs]
+  (reduce
+    (fn [[reached s] x]
+      (if (reached (+ s x))
+        (reduced (+ s x))
+        [(conj reached (+ s x)) (+ s x)]))
+    [#{} 0]
+    inputs))
 
 (->> "inputs/day-one.txt"
      slurp
      str/split-lines
      (map #(Integer/parseInt %))
+     (cycle)
      (reached_twice)
      )
 
 ; sample
-; (reached_twice [1 2 -1])
+(comment (loop_reached_twice [1 2 -1]))
+(comment (reached_twice [1 2 -1 1]))
