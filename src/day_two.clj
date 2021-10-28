6d2ff987c0d206c6d63867dade98c5f0082fa0bc
parkdoyeon <>
2021-10-28 16:17:35 +0900

    Add day-two part 2

--- src/day_two.clj
+++ src/day_two.clj
@@ -14,51 +14,54 @@
 
 (comment (char-count "aabc"))
 
-(defn map_has_target [target char_map]
-  (some (fn [[_ v]] (= v target)) char_map))
+(defn map-has-target [target char-map]
+  (some (fn [[- v]] (= v target)) char-map))
 
-; Warn! Walk traverses form, an **arbitrary** data structure
+; Warn! Walk traverses form, an *arbitrary* data structure
+; Not recommended for explicit data structure
 (comment
   (->> (str/split-lines (slurp "resources/day-two.txt"))
        (map char-count)
-       (walk/walk (fn [char_map]
-                    [(map_has_target 2 char_map), (map_has_target 3 char_map)]
+       (walk/walk (fn [char-map]
+                    [(map-has-target 2 char-map), (map-has-target 3 char-map)]
                     )
-                  #(reduce (fn [[two three], [has_two has_three]]
-                             [(if has_two (inc two) two), (if has_three (inc three) three)])
+                  #(reduce (fn [[two three], [has-two has-three]]
+                             [(if has-two (inc two) two), (if has-three (inc three) three)])
                            [0 0] %))
        (apply *))
   )
+
 ; Use frequencies
 ; Replace walk/walk as reduce
-(defn has_target [target char_map]
-  (some (fn [x] (= x target)) char_map))
+(defn has-target [target char-map]
+  (some (fn [x] (= x target)) char-map))
 
-((comment
-   (->> (str/split-lines (slurp "resources/day-two.txt"))
-        (map frequencies)
-        (map vals)
-        (reduce (fn [[two three] freq]
-                  [(if (has_target 2 freq) (inc two) two) (if (has_target 3 freq) (inc three) three)])
-                [0, 0])
-        (apply *)
-        ))
+(comment
+  (->> (str/split-lines (slurp "resources/day-two.txt"))
+       (map frequencies)
+       (map vals)
+       (reduce (fn [[two three] freq]
+                 [(if (has-target 2 freq) (inc two) two) (if (has-target 3 freq) (inc three) three)])
+               [0, 0])
+       (apply *)))
 
 
- ; part 2
- (defn has_one_diff [t1 t2]
-   (for [t1 t1 t2 t2])
-   )
+; part 2
+(defn common-chars [t1 t2]
+  (if (= t1 t2)
+    '(true)
+    (map (fn [a b] (when (= a b) a)) t1 t2)
+    ))
 
- (let [txt_list (->> "inputs/day-two.txt"
-                     slurp
-                     str/split-lines
-                     (map #(str/split % #"")))
-       ret ""]
-   (for [a txt_list
-         b txt_list
-         :when
-         (has_one_diff a b)]
-     [a, b]
-     )
-   )
+(comment (common-chars ["a" "b" "c" "e" "f"] ["a" "b" "c" "k" "f"]))
+
+(comment (def txt-list (->> (str/split-lines (slurp "resources/day-two.txt"))
+                   (map #(str/split % #"")))))
+
+(comment (->> (for [a txt-list
+                    b txt-list
+                    :let [common (common-chars a b)]
+                    :when (= (count (filter nil? common)) 1)]
+                common)
+              (first)
+              (apply str)))
