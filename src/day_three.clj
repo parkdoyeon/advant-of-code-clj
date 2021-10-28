054365fc9f7bda99ee1bf363207a1e94fe23671d
parkdoyeon <>
2021-10-28 23:24:18 +0900

    Add day three part 1

---
+++ src/day_three.clj
@@ -0,0 +1,65 @@
+(ns day_three
+  (:require [clojure.string :as str]))
+
+; Create template
+; (
+; {:id "#1", :left 2, :top 2, :width 5, :height 5},
+; {:id "#1", :left 2, :top 2, :width 5, :height 5},
+; ...)
+
+(defn parse-order [order-txt]
+  (let [[id _ inch size] (str/split order-txt #" ")]
+    {:id     id,
+     :left   (Integer/parseInt (subs inch 0 (str/index-of inch ","))),
+     :top    (Integer/parseInt (subs inch (+ (str/index-of inch ",") 1) (str/index-of inch ":"))),
+     :width  (Integer/parseInt (subs size 0 (str/index-of size "x"))),
+     :height (Integer/parseInt (subs size (+ (str/index-of size "x") 1)))}))
+
+(comment (def orders (->>
+                       (str/split-lines (slurp "resources/day-three.txt"))
+                       (map parse-order))))
+
+
+; part 1
+(defn mark-fabric [fabric template]
+  "
+  in
+    {:id \"#1\", :left 2, :top 2, :width 5, :height 5}
+  out
+  {2 {2 1, 3 1, 4 1, 5 1, 6 1},
+   3 {2 1, 3 1, 4 1, 5 1, 6 1},
+   4 {2 1, 3 1, 4 1, 5 1, 6 1},
+   5 {2 1, 3 1, 4 1, 5 1, 6 1},
+   6 {2 1, 3 1, 4 1, 5 1, 6 1}}
+  "
+  (let [left (get template :left)
+        width (get template :width)
+        top (get template :top)
+        height (get template :height)]
+    (reduce (fn [acc x]
+              (reduce (fn [in_acc y]
+                        (update-in in_acc [x y] (fnil inc 0)))
+                      acc
+                      (range left (+ left width))))
+            fabric
+            (range top (+ top height)))))
+
+(comment (mark-fabric {} {:id "#1", :left 2, :top 2, :width 5, :height 5}))
+
+(defn count-dup-orders [line]
+  (reduce (fn [acc order] (if (> order 1) (inc acc) acc)) 0 line))
+
+(comment
+  (->> (reduce #(mark-fabric %1 %2) {} orders)
+       (vals)
+       (map vals)
+       (map count-dup-orders)
+       (apply +)))
+
+; part 2
+;(comment
+;  (->> (reduce #(put-order %1 %2) {})
+;       (vals)
+;       (map vals)
+;       (map count-dup-orders)
+;       (apply +)))
