6a065c051e981944f43f955aa822d91455b8a43e
parkdoyeon <>
2021-10-28 14:07:16 +0900

    Fix formatting

--- src/day_one.clj
+++ src/day_one.clj
@@ -1,17 +1,17 @@
 (ns day-one
-   (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
 ; part 1
 ; Use apply
 (comment
-    (->> (str/split-lines (slurp "resources/day-one.txt"))
-    (map #(Integer/parseInt %))
-    (apply +)))
+  (->> (str/split-lines (slurp "resources/day-one.txt"))
+       (map #(Integer/parseInt %))
+       (apply +)))
 
 ; Use reducer
 (comment
-    (->> (str/split-lines (slurp "resources/day-one.txt"))
-     (reduce #(+ %1 (Integer/parseInt %2)) 0)))
+  (->> (str/split-lines (slurp "resources/day-one.txt"))
+       (reduce #(+ %1 (Integer/parseInt %2)) 0)))
 
 ; part 2
 ; Use recur
@@ -19,12 +19,12 @@
   (loop [sum 0
          inputs inputs
          reached #{}]
-      (if (reached sum)
-        sum
-        (recur (+ sum (first inputs))
-               (rest inputs)
-               (conj reached sum))
-        )))
+    (if (reached sum)
+      sum
+      (recur (+ sum (first inputs))
+             (rest inputs)
+             (conj reached sum))
+      )))
 
 ; Use reducer
 (defn reached-twice [inputs]
@@ -37,10 +37,10 @@
     inputs))
 
 (comment
-    (->> (str/split-lines (slurp "resources/day-one.txt"))
-     (map #(Integer/parseInt %))
-     (cycle)
-     (reached-twice)))
+  (->> (str/split-lines (slurp "resources/day-one.txt"))
+       (map #(Integer/parseInt %))
+       (cycle)
+       (reached-twice)))
 
 ; sample
 (comment (loop-reached-twice [1 2 -1]))
--- src/day_two.clj
+++ src/day_two.clj
@@ -6,29 +6,29 @@
 ; part 1
 ; Replace `frequencies`
 (defn char-count [txt]
-     (->> (str/split txt #"")
-          (reduce (fn [m c]
-                       (assoc m c (inc (get m c 0))))
-                  {})
-          ))
+  (->> (str/split txt #"")
+       (reduce (fn [m c]
+                 (assoc m c (inc (get m c 0))))
+               {})
+       ))
 
 (comment (char-count "aabc"))
 
 (defn map_has_target [target char_map]
-     (some (fn [[_ v]] (= v target)) char_map))
+  (some (fn [[_ v]] (= v target)) char_map))
 
 ; Warn! Walk traverses form, an **arbitrary** data structure
 (comment
-    (->> (str/split-lines (slurp "resources/day-two.txt"))
-     (map char-count)
-     (walk/walk (fn [char_map]
-                [(map_has_target 2 char_map), (map_has_target 3 char_map)]
-                )
-           #(reduce (fn [[two three], [has_two has_three]]
-                        [(if has_two (inc two) two), (if has_three (inc three) three)])
-                   [0 0] %))
-     (apply *))
-)
+  (->> (str/split-lines (slurp "resources/day-two.txt"))
+       (map char-count)
+       (walk/walk (fn [char_map]
+                    [(map_has_target 2 char_map), (map_has_target 3 char_map)]
+                    )
+                  #(reduce (fn [[two three], [has_two has_three]]
+                             [(if has_two (inc two) two), (if has_three (inc three) three)])
+                           [0 0] %))
+       (apply *))
+  )
 ; Use frequencies
 ; Replace walk/walk as reduce
 (defn has_target [target char_map]
@@ -36,29 +36,29 @@
 
 ((comment
    (->> (str/split-lines (slurp "resources/day-two.txt"))
-     (map frequencies)
-     (map vals)
-     (reduce (fn [[two three] freq]
-               [(if (has_target 2 freq) (inc two) two) (if (has_target 3 freq) (inc three) three)])
-             [0, 0])
-     (apply *)
-     ))
+        (map frequencies)
+        (map vals)
+        (reduce (fn [[two three] freq]
+                  [(if (has_target 2 freq) (inc two) two) (if (has_target 3 freq) (inc three) three)])
+                [0, 0])
+        (apply *)
+        ))
 
 
-; part 2
-;(defn has_one_diff [t1 t2]
-;     (for [t1 t1 t2 t2])
-;     )
-;
-;(let [txt_list (->> "inputs/day-two.txt"
-;               slurp
-;               str/split-lines
-;               (map #(str/split % #"")))
-;      ret ""]
-;     (for [a txt_list
-;           b txt_list
-;           :when
-;           (has_one_diff a b)]
-;          [a, b]
-;          )
-;     )
+ ; part 2
+ (defn has_one_diff [t1 t2]
+   (for [t1 t1 t2 t2])
+   )
+
+ (let [txt_list (->> "inputs/day-two.txt"
+                     slurp
+                     str/split-lines
+                     (map #(str/split % #"")))
+       ret ""]
+   (for [a txt_list
+         b txt_list
+         :when
+         (has_one_diff a b)]
+     [a, b]
+     )
+   )
