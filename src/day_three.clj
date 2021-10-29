f3118d52c313dbdeb3d30f7cd7e87ec20ef560e5
parkdoyeon <>
2021-10-29 10:22:41 +0900

    Add day three part 2

--- src/day_three.clj
+++ src/day_three.clj
@@ -1,5 +1,7 @@
 (ns day_three
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.walk :as walk]))
 
 ; Create template
 ; (
@@ -15,9 +17,11 @@
      :width  (Integer/parseInt (subs size 0 (str/index-of size "x"))),
      :height (Integer/parseInt (subs size (+ (str/index-of size "x") 1)))}))
 
+; Prepare global data
 (comment (def orders (->>
                        (str/split-lines (slurp "resources/day-three.txt"))
-                       (map parse-order))))
+                       (map parse-order)))
+         (def ids (into #{} (map #(get % :id) orders))))
 
 
 ; part 1
@@ -37,13 +41,14 @@
         top (get template :top)
         height (get template :height)]
     (reduce (fn [acc x]
-              (reduce (fn [in_acc y]
-                        (update-in in_acc [x y] (fnil inc 0)))
+              (reduce (fn [in-acc y]
+                        (update-in in-acc [x y] (fnil inc 0)))
                       acc
                       (range left (+ left width))))
             fabric
             (range top (+ top height)))))
 
+; sample
 (comment (mark-fabric {} {:id "#1", :left 2, :top 2, :width 5, :height 5}))
 
 (defn count-dup-orders [line]
@@ -51,15 +56,46 @@
 
 (comment
   (->> (reduce #(mark-fabric %1 %2) {} orders)
-       (vals)
+       vals
        (map vals)
        (map count-dup-orders)
        (apply +)))
 
-; part 2
-;(comment
-;  (->> (reduce #(put-order %1 %2) {})
-;       (vals)
-;       (map vals)
-;       (map count-dup-orders)
-;       (apply +)))
+
+;part 2
+(defn mark-fabric-id [fabric template]
+  "
+  in
+    {:id \"#1\", :left 2, :top 2, :width 2, :height 2}
+  out
+    {2 {2 #{\"#1\"},3 #{\"#1\"}},
+     3 {2 #{\"#1\"},3 #{\"#1\"}}}
+  "
+  (let [id (get template :id)
+        left (get template :left)
+        width (get template :width)
+        top (get template :top)
+        height (get template :height)]
+    (reduce (fn [acc x]
+              (reduce (fn [in-acc y]
+                        (update-in in-acc [x y] (fn [v]
+                                                  (if (nil? v)
+                                                    #{id}
+                                                    (conj v id)))))
+                      acc
+                      (range left (+ left width))))
+            fabric
+            (range top (+ top height)))))
+
+; sample
+(comment (-> (mark-fabric-id {} {:id "#1", :left 2, :top 2, :width 2, :height 2})
+             (mark-fabric-id {:id "#2", :left 2, :top 2, :width 2, :height 2})))
+
+(comment
+  (->> (reduce #(mark-fabric-id %1 %2) {} orders)
+       vals                                                 ; remove keys
+       (map vals)                                           ; remove inner keys
+       flatten                                              ; remove list between 2-dimension arrays
+       (filter #(> (count %) 1))                            ; remove fabric which has single orders
+       (into #{})                                           ; remove duplicate set elements
+       (reduce #(set/difference %1 %2) ids)))
