3d02684c31ed0595043115e1c7de80557da6c1bd
parkdoyeon <>
2021-11-11 09:22:06 +0900

    Refactor 2020 day 8 - replace string to keyword

--- src/twenty_day_eight.clj
+++ src/twenty_day_eight.clj
@@ -4,11 +4,11 @@
 (defn parse-instruction [line]
   (let [[operator str-arg] (str/split line #" ")
         num-arg (Integer/parseInt str-arg)]
-    [operator num-arg]))
+    [(keyword operator) num-arg]))
 
 (def instructions (->>
                     (str/split-lines (slurp "resources/twenty-day-eight.txt"))
-                    (mapv parse-instruction)))
+                    (map parse-instruction)))
 
 (defn initialize-status [instructions]
   {:histories    {}
@@ -21,34 +21,29 @@
          instructions :instructions} status
         [operator arg] (nth instructions (:idx current))]
     (case operator
-      "acc" (-> status
-                (assoc-in [:current :prev-sum] (:sum current))
-                (update-in [:current :idx] inc)
-                (update-in [:current :sum] + arg))
-      "jmp" (-> status
-                (update-in [:current :idx] + arg))
-      "nop" (-> status
-                (update-in [:current :idx] inc)))))
+      :acc (-> status
+               (assoc-in [:current :prev-sum] (:sum current))
+               (update-in [:current :idx] inc)
+               (update-in [:current :sum] + arg))
+      :jmp (-> status
+               (update-in [:current :idx] + arg))
+      :nop (-> status
+               (update-in [:current :idx] inc)))))
 
 (defn label-non-acc-as-cycle [status]
-  (let [{current      :current
-         instructions :instructions} status
-        current-idx (:idx current)
-        [operator _] (nth instructions current-idx)]
-    (if (= "acc" operator)
+  (let [current-idx (:idx (:current status))
+        [operator _] (nth (:instructions status) current-idx)]
+    (if (= :acc operator)
       status
       (assoc-in status [:histories current-idx] :cycle))))
 
 (defn fix [instructions fix-idx]
   (let [[operator arg] (nth instructions fix-idx)
-        new-operator (if (= "jmp" operator) "nop" "jmp")]
+        new-operator (if (= :jmp operator) :nop :jmp)]
     (assoc instructions fix-idx [new-operator arg])))
 
-(defn run-instruction [status]
-  (let [{current      :current
-         histories    :histories
-         instructions :instructions} status
-        current-idx (:idx current)
+(defn run-instruction [{:keys [current histories instructions] :as status}]
+  (let [current-idx (:idx current)
         history (histories current-idx)]
     (if (= current-idx (dec (count instructions)))
       (-> status
@@ -63,8 +58,7 @@
                   label-non-acc-as-cycle
                   operate)
         :cycle (-> status
-                   (assoc :flag :in-loop)
-                   operate)))))
+                   (assoc :flag :in-loop))))))
 
 (defn run-program [exit-when instructions]
   (->> (initialize-status instructions)
@@ -75,8 +69,8 @@
 (defn get-maybe-broken-idx [status]
   (->> status
        :histories
-       (filterv #(= :cycle (second %)))
-       (mapv first)))
+       (filter #(= :cycle (second %)))
+       (map first)))
 
 (comment
   ; test
@@ -90,14 +84,15 @@
        :prev-sum)
 
   ; part 2
-  (->> instructions
-       (run-program #{:in-loop})
-       get-maybe-broken-idx
-       (map #(fix instructions %))
-       (map #(run-program #{:in-loop :success} %))
-       (filter #(= (:flag %) :success))
-       first
-       :current
-       :sum))
+  (let [fixed-instructions (->> instructions
+                                (run-program #{:in-loop})
+                                get-maybe-broken-idx
+                                (map #(fix instructions %)))]
+    (->> fixed-instructions
+         (map #(run-program #{:in-loop :success} %))
+         (filter #(= (:flag %) :success))
+         first
+         :current
+         :sum)))
 
 
